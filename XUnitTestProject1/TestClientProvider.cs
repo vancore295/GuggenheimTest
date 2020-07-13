2029a00aec58f3d4c912a2328c5d524e9f449f36
vancore295 <silverfire295@gmail.com>
2020-07-13 08:30:36 -0400

    added test client

---
+++ XUnitTestProject1/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using GuggenheimTest;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace XUnitTestProject1
+{
+    class TestClientProvider
+    {
+        public HttpClient Client { get; private set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
