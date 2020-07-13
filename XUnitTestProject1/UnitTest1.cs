87c0753d1dc2cf7dcc7698714dcc57868ac04985
vancore295 <silverfire295@gmail.com>
2020-07-13 09:04:06 -0400

    post test

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,10 @@
+using GuggenheimTest.Models;
+using Newtonsoft.Json;
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace XUnitTestProject1
@@ -6,9 +12,22 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task Test_Post()
         {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.PostAsync("/Trip", new StringContent(JsonConvert.SerializeObject(
+                    new StaticTrip
+                    {
+                        date = DateTime.Parse("2020-07-17"),
+                        milesUnder6 = 2,
+                        minAbove6 = 5,
+                        time = "17:30"
+                    }), Encoding.UTF8, "application/json"));
 
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
         }
     }
 }
